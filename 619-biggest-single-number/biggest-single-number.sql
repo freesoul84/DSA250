dac179a449985b733b331ad56b7864a984d1af01
Alkesha Baikar <baikaralkesha@gmail.com>
2025-04-25 12:11:29 +0530

    Time: 444 ms (68.88%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,6 +1,16 @@
 # Write your MySQL query statement below
+/*
 SELECT MAX(A.num) AS num
 FROM (SELECT num 
 FROM MyNumbers 
 GROUP BY num 
 HAVING COUNT(num) = 1) A
+*/
+
+SELECT COALESCE((SELECT num 
+FROM MyNumbers
+GROUP BY num
+HAVING COUNT(num) =1
+ORDER BY num DESC
+LIMIT 1),null)
+AS num
