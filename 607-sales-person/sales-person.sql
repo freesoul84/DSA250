172ab5dc6a80c9ac4d11d41ab50c6bd4255c669a
Alkesha Baikar <baikaralkesha@gmail.com>
2025-04-30 10:34:40 +0530

    Time: 622 ms (69.02%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+with CTE1 as (
+    select b.sales_id from Company a
+    join Orders b on a.com_id = b.com_id
+    where a.name = 'RED'
+)
+
+select a.name from SalesPerson a
+where a.sales_id not in (select sales_id from CTE1)
